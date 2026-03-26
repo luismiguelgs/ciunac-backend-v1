d7be27ff00399816c370c15cfee48ca6790d4674
Luis Gastiaburu <luismiguel.gs@outlook.com>
2026-03-26 10:54:01 -0500

    Version producción 1.0.7

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,15 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AppService {
-	constructor(private jwtService: JwtService) {}
+	constructor(private jwtService: JwtService) { }
 
 	getHello(): string {
-		return 'Hello World from Docker!';
+		return 'Hello World from Docker CIUNAC V1!';
 	}
 
 	generarToken(payload: any) {
 		const data = { sub: 1, username: payload.user }; // 👈 simulamos un usuario con id=1
-  		return this.jwtService.sign(data);
+		return this.jwtService.sign(data);
 	}
 
 	validarToken(token: string) {
